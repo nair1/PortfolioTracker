9769c31c9baf06b2c539b077340f19bc2bfb5117
nair1 <nair49@purdue.edu>
2023-03-04 23:59:36 -0600

    added basic DB connection to postgreSQL

---
+++ src/constants.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	DB_USERNAME = "DB_username"
+	DB_PASSWORD = "DB_password"
+	DB_HOST     = "localhost"
+	DB_PORT     = 5432
+	DB_NAME     = "PortfolioTracker"
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	username := os.Getenv(DB_USERNAME)
+	password := os.Getenv(DB_PASSWORD)
+
+	psqlConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, username, password, DB_NAME)
+
+	db, err := sql.Open("postgres", psqlConn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to the database!")
 }
